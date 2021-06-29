d53c88aab2bef9ad3ba49e579126c1c9f51106ff
anandkokate <86236826+anandkokate@users.noreply.github.com>
2021-06-29 17:36:07 +0530

    Add files via upload

---
+++ BloomfilterFalsePositiviteprob.py
@@ -0,0 +1,48 @@
+import hashlib
+import base64
+import mmh3
+class BloomFilter:
+    def __init__(self, m, k):
+        self.m = m
+        self.k = k
+        self.data = [0]*m
+        self.n = 0
+    def insert(self, element):
+        if self.k == 1:
+            
+            hash1 = mmh3.hash(element,41) % self.m
+            self.data[hash1] = 1
+        elif self.k == 2:
+            hash1 = mmh3.hash(element,41) % self.m
+            hash2 = mmh3.hash(element,42) % self.m
+            self.data[hash1] = 1
+            self.data[hash2] = 1
+        self.n += 1
+    def search(self, element):
+        
+        if self.k == 1:
+            hash1 = mmh3.hash(element,41) % self.m
+            if self.data[hash1] == 0:
+                return "Not in Bloom Filter"
+        elif self.k == 2:
+            
+            hash1 = mmh3.hash(element,41) % self.m
+            hash2 = mmh3.hash(element,42) % self.m
+            if self.data[hash1] == 0 or self.data[hash2] == 0:
+                return "Not in Bloom Filter"
+        prob = (1.0 - ((1.0 - 1.0/self.m)**(self.k*self.n))) ** self.k
+        return "Might be in Bloom Filter with false positive probability "+str(prob)
+
+bloom=BloomFilter(50,1)
+bloom.insert("Hello")
+#print(bloom.data)
+bloom.insert("cat")
+bloom.insert("mat")
+#bloom.insert('lo')
+print(bloom.data)
+b=bloom.search("cat")
+print(b)
+
+
+
+
